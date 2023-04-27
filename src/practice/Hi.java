7f7b2cdd0c00ee6160eeea566fa5aeaf8301041b
lucky <lucky@LAPTOP-JKGC4BSE>
2023-04-27 12:43:51 -0400

    uploading my files to github

---
+++ src/practice/Hi.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Titili");
+		System.out.println(80-20);
+		System.out.println("Hello world");
+	}
+
+}
